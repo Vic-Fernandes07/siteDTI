dbfbc8222ce5538100a3bef32e20222b51ebbbda
Sophia <almeidasophia086@gmail.com>
2024-09-26 11:16:37 -0300

    att da palhaça

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,21 +1,29 @@
 import "./index.css";
 import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
-
 import * as ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Exemplo from "./exemplo.jsx";
+import cabide from "./assets/cabide.png"
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <h1>Erro</h1>,
+    errorElement:  <>
+    <img src={cabide} alt="Imagem" />
+    <img src="" alt="" />
+    <h2>Oops! Algo deu errado.</h2>
+    <p>Essa pagina não existe</p>
+    </>,
+ 
     children: [
       { path: "/footer", element: <Footer /> },
       { path: "/ola", element: <Exemplo /> },
+      { path: "/olas", element: <olas /> },
     ],
   },
 ]);
---
+++ site/src/olas.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Exemplo = () => {
+  return <div>
+    <h1>Olá pagina 2👌</h1>
+  </div>;
+};
+
+export default Exemplo;
