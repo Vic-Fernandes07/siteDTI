51b07070f39f9cf94dca198bf408e1d81d710457
Cay <souzacaitto@gmail.com>
2024-09-26 08:47:43 -0300

    att icon

--- site/src/components/footer/Footer.jsx
+++ site/src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer-content">
-                <p>&copy; 2024 Sua Empresa. Todos os direitos reservados.</p>
+                <p>&copy; 2024 Dress To Impress. Todos os direitos reservados.</p>
                 <ul className="social-links">
                     <li><a href="#">Git</a></li>
                     <li><a href="#">E-mail</a></li>
