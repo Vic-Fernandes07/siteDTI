ba6bd3a40a8e658de7723873563bd84826d921f1
Vic-Ronchi <vitoria.ronchi7@gmail.com>
2024-09-27 10:04:58 -0300

    att coisinhas

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -3,14 +3,24 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import { Outlet } from "react-router-dom";
 import Login from "./components/login/Login";
+import Picture from "./assets/dresstoimpress.png"
 
 function App() {
   return (
     <div>
       <Header />
+      <div>
+        <img className="picture" src={Picture} alt="" />
+        <br />
+        <h2 className="texto">Bem-vindos!
+        <br />
+        A Dress To Impress fica feliz em te receber👋</h2>
+        <p></p>
+      </div>
       <Outlet />
       <Footer />
     </div>
+    
   );
 }
 
--- site/src/components/header/Header.jsx
+++ site/src/components/header/Header.jsx
@@ -5,6 +5,7 @@ import Closet from "../../assets/closet.png";
 import Book from "../../assets/book.png";
 import People from "../../assets/people.png";
 import icon from "../../assets/icon.png";
+import Carrinho from "../../assets/Group.png"
 import { Link } from "react-router-dom";
 
 export const Header = () => {
@@ -13,11 +14,11 @@ export const Header = () => {
       <div className="d-block bg-dark text-light">
         <div className="row m-0 divMenu m-0">
           <div className="col m-0 bg-black">
-            <Link to={"closet"}>
-              <img className="closet" src={Closet} alt="Closet icon" />
-            </Link>
             <Link to={"/"}>
               <img className="home" src={home} alt="Home icon" />
+            </Link>
+            <Link to={"closet"}>
+              <img className="closet" src={Closet} alt="Closet icon" />
             </Link>
             <Link to={"book"}>
               <img className="book" src={Book} alt="Book icon" />
@@ -28,6 +29,10 @@ export const Header = () => {
             <Link>
               <img className="icon" src={icon} alt="Notification icon" />
             </Link>
+            <Link to={"carrinho"}>
+            <img className="carrinho" src={Carrinho} alt="Carrinho icon" />
+            </Link>
+
           </div>
         </div>
       </div>
