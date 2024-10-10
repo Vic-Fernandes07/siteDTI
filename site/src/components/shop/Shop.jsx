6c202935feb4b426fc5f9dd44185d8e7bb7ddeb7
Sophia <almeidasophia086@gmail.com>
2024-10-10 14:36:41 -0300

    att sophi

--- site/src/components/shop/Shop.jsx
+++ site/src/components/shop/Shop.jsx
@@ -1,11 +1,15 @@
-// ProductPage.jsx
 import React from 'react';
-import "./Shop.css";
+import "../shop/Shop.css";
 import tenispreto from "../../assets/tenispreto.png"
 import tenisazul from "../../assets/tenisazul.png"
 import tenisnike from "../../assets/tenisnike.png"
+import camiseta from "../../assets/camisah.png"
+import calca from "../../assets/calca.png"
+import moletom from "../../assets/moletom.png"
+import pochete from "../../assets/pochete.png"
+import nike from "../../assets/nikec.png"
       
-const ProductPage = () => {
+const Shop = () => {
   const handleBuyClick = (productName, productPrice) => {
     alert(`Você está comprando: ${productName} por ${productPrice}. Obrigado por escolher a Dress To Impress!`);
   };
@@ -30,6 +34,36 @@ const ProductPage = () => {
           price: 'R$ 849,99',
           image: tenisnike,
         },
+        {
+          id: 'camiseta-unissex-nike',
+          title: 'Camiseta Nike SB Max90',
+          price: "R$ 237,49",
+          image: nike,
+        }, 
+        {
+          id: 'cargo- leve', 
+          title: 'Calça cargo leves Balenciaga', 
+          price: 'R$ 400,99', 
+          image: calca, 
+        }, 
+        { 
+          id: 'moletom', 
+          title: 'Moletom masculino Fleece', 
+          price: 'R$ 500,90', 
+          image: moletom, 
+        }, 
+        { 
+          id: 'pochete', 
+          title: 'Palm Angels Pack', 
+          price: 'R$ 259, 99', 
+          image: pochete,
+        },
+        {
+          id: 'camiseta',
+          title: 'Palm Angeles T-shirt', 
+          price: 'R$ 395,90', 
+          image: camiseta,
+        },
       ];
       
 
@@ -37,7 +71,7 @@ const ProductPage = () => {
     <div>
       <header className='mt-5'>
         <h1>Dress To Impress</h1>
-        <p>Sua loja especializada em moda Dress To Impress</p>
+        <p>Sua loja especializada em moda !</p>
       </header>
       <main>
         <div className="product-grid ">
@@ -62,4 +96,4 @@ const ProductPage = () => {
   );
 };
 
-export default ProductPage;
+export default Shop;
