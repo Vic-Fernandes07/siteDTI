f749397e9a5d3e6731399fb870d2460a080650b4
Cay <souzacaitto@gmail.com>
2024-10-10 07:49:22 -0300

    aaaa

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,35 +1,19 @@
-import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/home/Home";  
+import { Outlet, Route, Routes } from "react-router-dom";
 import Login from "./components/login/Login";
-import Carrinho from "./components/carrinho/Carrinho";
+import Picture from "./assets/dresstoimpress.png";
+import Profile from "./components/profile/perfil";
 
 function App() {
-  const [carrinho, setCarrinho] = useState([]);
-
-  const adicionarAoCarrinho = (produto) => {
-    setCarrinho([...carrinho, produto]);
-  };
-
-  const removerItem = (index) => {
-    const novoCarrinho = carrinho.filter((_, i) => i !== index);
-    setCarrinho(novoCarrinho);
-  };
-
   return (
     <div>
-      <Header itensCarrinho={carrinho} removerItem={removerItem} />
-      <Routes>
-        <Route path="/" element={<Home />} />  {/* Certifique-se de ter um componente Home */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/carrinho" element={<Carrinho itens={carrinho} removerItem={removerItem} />} />
-      </Routes>
+      <Header />
+      <Outlet />
       <Footer />
     </div>
   );
 }
 
-export default App;
+export default App; 
